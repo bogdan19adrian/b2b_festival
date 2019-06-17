b9522684721e5ff49e734dd68a6ddb08c8f6d230
bogdan19adrian <bogdan19adrian19adrian@gmail.com>
2019-06-17 23:07:31 +0300

    added price on admin and fe

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 
 # Register your models here.
-from main.models import Carousel, ConfirmedArtists
+from main.models import Carousel, ConfirmedArtists, Pricing
 
 admin.site.register(Carousel)
 admin.site.register(ConfirmedArtists)
+admin.site.register(Pricing)
--- main/views.py
+++ main/views.py
@@ -3,14 +3,16 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-from main.models import Carousel, ConfirmedArtists
+from main.models import Carousel, ConfirmedArtists, Pricing
 
 
 def homepage(request):
     return render(request=request,
                   template_name='main/home.html',
                   context={"carousels": Carousel.objects.all,
-                           "confirmed_artists": ConfirmedArtists.objects.all})
+                           "confirmed_artists": ConfirmedArtists.objects.all,
+                           "pricing": Pricing.objects.all
+                           })
 
 
 def confirmed_artists(request):
