7aa398578bf4f10530756137109c299915e9f5f9
bogdan19adrian <bogdan19adrian19adrian@gmail.com>
2019-09-22 14:53:31 +0300

    adding mysql

--- b2b_festival/settings.py
+++ b2b_festival/settings.py
@@ -72,10 +72,19 @@ WSGI_APPLICATION = 'b2b_festival.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'D:\\WorkingPrograms\\db-cnf',
+        },
     }
 }
 
