2e69bd482fba573d3bb6cb64df2dbe6cb2cf5eb8
enacbog <bogdan19adrian@gmail.com>
2020-03-14 13:40:55 +0200

    working carousel

--- b2b_festival/settings.py
+++ b2b_festival/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': 'D:\\WorkingPrograms\\db-cnf',
+            'read_default_file': 'db.cnf',
         },
     }
 }
