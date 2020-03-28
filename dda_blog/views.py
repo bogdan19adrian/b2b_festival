6185e182d4fddbf6021097312f9eb03b639fb1f4
enacbog <bogdan19adrian@gmail.com>
2020-03-28 18:03:36 +0200

    added tinymce

--- dda_blog/views.py
+++ dda_blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.decorators.cache import cache_control
 from django.views.decorators.vary import vary_on_headers
@@ -5,36 +6,28 @@ from django.views.decorators.vary import vary_on_headers
 from .models import Post
 
 
-# class PostList(generic.ListView):
-#     queryset = Post.objects.filter(status=1).order_by('-created_on')
-#     template_name = 'dda_blog/dda_blog.html'
-#
-#
-# class PostDetail(generic.DetailView):
-#     model = Post
-#     template_name = "dda_blog/dda_blog_detail.html"
-
-
-# class FilteredPostList(generic.DetailView):
-#     queryset = Post.objects.filter(status=1, category=1).order_by('-created_on')
-@cache_control(private=True, max_age=3600)
-@vary_on_headers('User-Agent')
+# @cache_control(private=True, max_age=3600)
+# @vary_on_headers('User-Agent')
 def blog_main_mage(request):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     filtered_post_list = Post.objects.filter(status=1, category=1).order_by('-created_on')
+
+    paginator = Paginator(queryset, 10) # Show 10 Posts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(request=request,
                   template_name='dda_blog/dda_blog.html',
-                  context={"post_list": queryset,
-                           "filtered_post_list": filtered_post_list
+                  context={"filtered_post_list": filtered_post_list, 'page_obj': page_obj
                            })
 
 
-@cache_control(private=True, max_age=3600)
-@vary_on_headers('User-Agent')
+# @cache_control(private=True, max_age=3600)
+# @vary_on_headers('User-Agent')
 def blog_details_page(request, slug):
     post_list = Post.objects.filter(status=1).order_by('-created_on')
-    # post = Post.objects.filter(title__exact=str(slug))
-    post = Post.objects.all()[0]
+    post = Post.objects.get(slug=slug)
+    # post = Post.objects.all()[0]
     return render(request=request,
                   template_name='dda_blog/dda_blog_detail.html',
                   context={"post": post,
